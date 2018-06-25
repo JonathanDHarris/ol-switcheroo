fc2a2284323f86c0116fb0ab38582a4c0eba9421
Jon Harris <jharris@scottlogic.com>
2018-06-25 12:06:33 +0100

    Comment support

--- app.js
+++ app.js
@@ -17,18 +17,22 @@ const createBody = (posts, comments) => {
 	return `<body><table><td valign="top " style="width:50%">${renderPosts(posts)}</td><td valign="bottom">${renderComments(comments)}</td></table></body>`
 }
 
-const renderComments = (comments, depth) => {
-	if (!comments) {
+const renderComments = (comments, depth=1) => {
+	if (!comments || depth > maxCommentDepth) {
 		return;
 	}
 	
-	const margin = depth ? depth : 10
+	const margin = parseInt(depth, 10) * 10;
 	let html = `<div style="margin-left:${margin}px">`;
 
 	comments.forEach(comment => {
 		html += '' + unescape(comment.data.body_html)
 		html += `<i>${comment.data.author}</i>`
 		html += `</br>`
+		if (comment.data.replies && comment.data.replies.data && comment.data.replies.data.children) {
+			const replies = renderComments(comment.data.replies.data.children, depth+1);
+			html += replies;
+		}
 	})
 
 	html += `</div>`
@@ -55,17 +59,19 @@ const renderPost = post => {
 		html += `<span>${post.subreddit}</span>`
 	}
 	html += `</br>`
-	html += `<a href=?comments=${post.comments}>comments</a>`
+	html += `<a href=?subreddit=${post.subreddit}&comments=${post.comments}>comments</a>`
 	html += `</li>`
 	return html;
 }
 
 const MAIN = `https://www.reddit.com/`
 
-const fetchUrl = async(url, limit, after) => {
+const maxCommentDepth = 5;
+
+const fetchUrl = async(url, subreddit, limit, after) => {
 	const fetchLimit = limit ? limmit : 10;
 	const fetchAfter = after ? after : 0;
-	const fetchUrl = `${url}.json?limit=${fetchLimit}&after=${fetchAfter}`;
+	const fetchUrl = `${url}${subreddit}.json?limit=${fetchLimit}&after=${fetchAfter}`;
 	console.log(`Fetching ${fetchUrl}`);
 	const response = await fetch(fetchUrl);
 	const responseJson = await response.json();
@@ -81,8 +87,8 @@ const fetchComments = async(commentUrl) => {
 }
 
 app.get('/', asyncMiddleware(async (req, res) => {
-	console.log('query', req.query);
-	const responseJson = await fetchUrl(MAIN);
+	const subreddit = req.query.subreddit ? `r/${req.query.subreddit}` : '';
+	const responseJson = await fetchUrl(MAIN, subreddit);
 	
 	const children = responseJson.data.children;
 	
