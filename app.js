0722e1083033ef2ee41d2fbe74a3eb15239d3ea7
Jon Harris <jharris@scottlogic.com>
2018-06-25 09:40:59 +0100

    First working async call

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const fetch = require('isomorphic-fetch')
+const app = express()
+
+const asyncMiddleware = fn =>
+  (req, res, next) => {
+    Promise.resolve(fn(req, res, next))
+      .catch(next);
+  };
+
+const MAIN = `https://www.reddit.com/`
+
+const fetchUrl = async(url, limit, after) => {
+	const fetchLimit = limit ? limmit : 10;
+	const fetchAfter = after ? after : 0;
+	const fetchUrl = `${url}.json?limit=${fetchLimit}&after=${fetchAfter}`;
+	console.log(`Fetching ${fetchUrl}`);
+	const response = await fetch(fetchUrl);
+	const responseJson = await response.json();
+	return responseJson;
+}
+
+app.get('/', asyncMiddleware(async (req, res) => {
+	const responseJson = await fetchUrl(MAIN);
+	console.log(responseJson);
+	res.send(responseJson);
+}))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
